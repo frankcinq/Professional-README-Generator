a731d5ca5c43d8e00bb3d207fe7ce14ab05f3e7c
Francisco Almaraz <frankcinq@yahoo.com>
2021-09-29 23:52:09 -0500

    final updates

---
+++ Develop/README.md
@@ -0,0 +1,34 @@
+![GitHub license](https://img.shields.io/badge/license--blue.svg)
+
+# ReadME
+
+## Description
+
+readme generator
+
+## Table of Contents
+
+- [Installation](Installation)
+- [Usage](Usage)
+- [Contributions](Contributions)
+- [Tests](Tests)
+- [Questions](Questions)
+  <br>
+
+## <a name = "Installation">Installation</a>
+
+install
+
+## <a name = "Usage">Usage</a>
+
+none
+
+## <a name = "Contributions">Contributing</a>
+
+frankcinq
+
+## <a name = "Tests">Tests</a>
+
+test questions
+
+## <a name = "Questions">Questions</a>
--- Develop/index.js
+++ Develop/index.js
@@ -1,45 +1,78 @@
-// TODO: Include packages needed for this application
-const inquirer = require("inquirer"); // this brings in a node package in your node modules folder
-const fs = require("fs"); // this is already packaged with node.js and just needs to be imported
-const generateMarkdown = require("./utils/generateMarkdown"); // thjis was exported from a file in your directory, make sure name and path are correct
-
-// TODO: Create an array of questions for user input
-const questions = [
-  {
-    type: "input",
-    name: "title",
-    message: "What is your projects title?",
-  },
-  {
-    type: "checkbox",
-    message: "What languages do you know?",
-    name: "stack",
-    choices: ["HTML", "CSS", "JavaScript", "MySQL"],
-  },
-  {
-    type: "list",
-    message: "What is your preferred method of communication?",
-    name: "contact",
-    choices: ["email", "phone", "telekinesis"],
-  },
-];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {
-  inquirer
-    .prompt(questions)
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (err) {
-      if (err) console.log(err);
-    });
-}
-
-// Function call to initialize app
-init();
-
-//  good luck
+const inquirer = require("inquirer");
+const fs = require("fs");
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "what is the name of your project?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Please enter a description of your project:",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "Please enter the installation instructions:",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "Please enter the contribution guidelines:",
+      name: "contribution",
+    },
+    {
+      type: "input",
+      message: "Please enter usage information:",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "Please enter test instructions:",
+      name: "test",
+    },
+    {
+      type: "checkbox",
+      message: "Plese select a license from the options:",
+      name: "license",
+      pageSize: 4,
+      highlight: true,
+      searchable: true,
+      choices: [
+        "MIT",
+        "Apache Licesnse 2.0",
+        "GNU General Public License v3.0",
+        "BSD 2-Clause 'Simplified' License",
+      ],
+    },
+  ])
+  .then((response) => writeNewReadMe(response));
+var writeNewReadMe = function (response) {
+  fs.writeFile("README.md", writeReadMe(response), (err) =>
+    err ? console.error(err) : console.log("successfully written README.md!")
+  );
+};
+const writeReadMe = function (response) {
+  const readMe = `![GitHub license](https://img.shields.io/badge/license-${response.license}-blue.svg)
+  # ${response.title}
+  ## Description
+  ${response.description}
+  ## Table of Contents
+* [Installation](Installation)
+*  [Usage](Usage)
+* [Contributions](Contributions)
+* [Tests](Tests)
+* [Questions](Questions)
+<br>
+## <a name = "Installation">Installation</a>
+  ${response.installation}
+  ## <a name = "Usage">Usage</a>
+  ${response.usage}
+  ## <a name = "Contributions">Contributing</a>
+  ${response.contribution}
+  ## <a name = "Tests">Tests</a>
+  ${response.test}
+  ## <a name = "Questions">Questions</a>`;
+  return readMe;
+};
